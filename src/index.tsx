3e5ecafc2e8b1e226f6d05c46972e382f9374bda
abanoubm1234567 <amasoud1234567@gmail.com>
2025-07-13 19:43:53 -0400

    first(ish) commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import HomeScreen from "./pages/HomeScreen";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <HomeScreen />
   </React.StrictMode>
 );
 
---
+++ src/pages/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import "../styles/HomeScreen.css";
+
+function HomeScreen() {
+  return (
+    <div className="HomeScreen">
+      <h1 className="title">Welcome</h1>
+      <button className="button>">Create New Lobby</button>
+      <button className="button>">Join Exisiting</button>
+    </div>
+  );
+}
+
+export default HomeScreen;
