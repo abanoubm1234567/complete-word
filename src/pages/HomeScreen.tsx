cbbbcd6a1abecba9f0764a140e1770992701df67
abanoubm1234567 <amasoud1234567@gmail.com>
2025-07-13 19:59:19 -0400

    homescreen ui complete

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -1,11 +1,24 @@
 import "../styles/HomeScreen.css";
+import React, { useState } from "react";
 
 function HomeScreen() {
+  const [lobbyKey, setLobbyKey] = useState("");
+
   return (
     <div className="HomeScreen">
-      <h1 className="title">Welcome</h1>
-      <button className="button>">Create New Lobby</button>
+      <h1 className="title">Word Race</h1>
+
+      <input
+        type="text"
+        className="lobbyKeyInput"
+        placeholder="Enter lobby key"
+        onChange={(e) => setLobbyKey(e.target.value)}
+      ></input>
+
       <button className="button>">Join Exisiting</button>
+
+      <p>or</p>
+      <button className="button>">Create New Lobby</button>
     </div>
   );
 }
