3326f79cc111a1a4d563ccfbfc4b8ffbd67106ca
abanoubm <amasoud1234567@gmail.com>
2025-07-28 17:33:32 -0400

    added error if display name is empty

--- ui/src/pages/HomeScreen.tsx
+++ ui/src/pages/HomeScreen.tsx
@@ -4,11 +4,26 @@ import { useNavigate } from "react-router-dom";
 import "../styles/HomeScreen.css";
 
 function HomeScreen() {
-  const joinLobbyKey = useRef("");
+  const joinLobbyKey = useRef<String>("");
+  const displayName = useRef<String>("");
+
   const nav = useNavigate();
 
+  const [displayNameError, setDisplayNameError] = useState<boolean>(false);
+
   const handleCreate = () => {
+    if (displayName.current === "") {
+      setDisplayNameError(true);
+      return;
+    }
     nav("/create");
+  };
+
+  const handleDisplayNameChange = (value: String) => {
+    if (displayNameError && value !== "") {
+      setDisplayNameError(false);
+    }
+    displayName.current = value;
   };
 
   return (
@@ -22,28 +37,51 @@ function HomeScreen() {
       }}
     >
       <h1 className="title">Word Race</h1>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <div className="input-group mb-3" style={{ width: "75vw" }}>
+          <span className="input-group-text" id="basic-addon1">
+            Display Name
+          </span>
 
-      <div className="input-group mb-3" style={{ width: "50%" }}>
-        <span className="input-group-text" id="basic-addon1">
-          Lobby Key:
-        </span>
+          <input
+            type="text"
+            className="form-control"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+            onChange={(e) => handleDisplayNameChange(e.target.value)}
+          />
+        </div>
+        {displayNameError ? (
+          <p style={{ color: "red" }}>Display name cannot be empty.</p>
+        ) : null}
+        <div className="input-group mb-3" style={{ width: "75vw" }}>
+          <span className="input-group-text" id="basic-addon1">
+            Lobby Key
+          </span>
 
-        <input
-          type="text"
-          className="form-control"
-          aria-label="Username"
-          aria-describedby="basic-addon1"
-          onChange={(e) => (joinLobbyKey.current = e.target.value)}
-        />
+          <input
+            type="text"
+            className="form-control"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+            onChange={(e) => (joinLobbyKey.current = e.target.value)}
+          />
+        </div>
+
+        <button style={{ marginBottom: 20 }} className="btn btn-success">
+          Join
+        </button>
+
+        <button className="btn btn-primary" onClick={handleCreate}>
+          Create New Lobby
+        </button>
       </div>
-
-      <button style={{ marginBottom: 20 }} className="btn btn-success">
-        Join
-      </button>
-
-      <button className="btn btn-primary" onClick={handleCreate}>
-        Create New Lobby
-      </button>
     </div>
   );
 }
